a6e4155b9aacd454c1b4338a9ff616119ce39847
moyen75 <moyenislam75@.com>
2021-11-30 22:29:12 +0600

    add a little link

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 import '../Header/Header.css'
 
 const Banner = () => {
+    const navigate = useNavigate()
+    const handleClick = () => {
+        navigate('/services')
+    }
     return (
         <div className='top-banner'>
             <Container className='main-banner'>
@@ -12,7 +17,7 @@ const Banner = () => {
                         <h4> MERN stack Developer  || Graphics Designer.</h4>
                     </div>
                     <p>Experienced Freelance with a demonstrated history of working in the education management industry. Skilled in Node.js, React Native, Front-end Development, Bootstrap, and Cascading Style Sheets (CSS). </p>
-                    <button className='button'>view my work</button>
+                    <button className='button' onClick={handleClick}>view my work</button>
                 </div>
             </Container>
         </div>
